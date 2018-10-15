e4c62a091bd0b594772d38c48e52e8dbdede8aab
Pavel_Z <zubrytski.pavel@gmail.com>
2018-10-15 19:18:35 +0300

    PagingNavigation

---
+++ PhoneStory/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneStory.Models;
+
+namespace PhoneStory.Controllers
+{
+    public class HomeController : Controller
+    {
+   
+        MobileContext db = new MobileContext();
+
+        public ActionResult Index(int page = 1)
+        {
+            int pageSize = 3;
+            ICollection<Phone> phonePerPage = db.Phones.OrderBy(p => p.Id).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            PageInfo pageInfo = new PageInfo { PageNumber = page, PageSize = pageSize, TotalItems = db.Phones.Count()};
+            IndexViewModel ivm = new IndexViewModel {PageInfo = pageInfo, Phones = phonePerPage };
+            return View(ivm);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PhoneStory/Models/PageInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneStory.Models
+{
+    public class PageInfo
+    {
+        public int PageNumber { get; set; } //номер  текущей страницы
+        public int PageSize { get; set; } //кол объектов на странице
+        public int TotalItems { get; set; } //всего объектов
+        public int TotalPages
+        {             //всего страниц
+            get
+            {
+                return (Int32)Math.Ceiling((Decimal)TotalItems / PageSize);
+            }
+        }
+    }
+
+    public class IndexViewModel {
+
+        public PageInfo PageInfo { get; set; }
+        public ICollection<Phone> Phones { get; set; }
+    }
+        
+
+}
